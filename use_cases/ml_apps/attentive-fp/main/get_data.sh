14a7b4e39eeca0fd725d10202aa7e5b80200ddf7
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2021-06-12 00:19:45 -0500

    attentive-fp module

--- use_cases/ml_apps/attentive-fp/main/get_data.sh
+++ use_cases/ml_apps/attentive-fp/main/get_data.sh
@@ -2,3 +2,4 @@ wget -c https://raw.githubusercontent.com/OpenDrugAI/AttentiveFP/master/data/mal
 wget -c https://raw.githubusercontent.com/OpenDrugAI/AttentiveFP/master/data/delaney-processed.csv -O dataset/delaney-processed.csv
 wget -c https://raw.githubusercontent.com/OpenDrugAI/AttentiveFP/master/data/cep-processed.csv -O dataset/cep-processed.csv
 wget -c https://raw.githubusercontent.com/OpenDrugAI/AttentiveFP/master/data/bace.csv -O dataset/bace.csv
+wget -c https://raw.githubusercontent.com/OpenDrugAI/AttentiveFP/master/data/HIV.csv -O dataset/HIV.csv
