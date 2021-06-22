4a6b00187a14ce1ce78e873ac6c2182822ed43c0
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2021-06-22 09:39:56 -0500

    modified attentive-fp

--- use_cases/ml_apps/attentive-fp/main/physical_chemistry_freesolv.py
+++ use_cases/ml_apps/attentive-fp/main/physical_chemistry_freesolv.py
@@ -41,25 +41,20 @@ from network.AttentiveFP.AttentiveLayers import Fingerprint
 
 import config, wandb
 
-wandb.init(project="photovoltaic-efficiency")
+wandb.init(project="physical-chemistry-freesolv")
 wandb.log({"run_dir": wandb.run.dir})
 
-task_name = 'Photovoltaic efficiency'
-tasks = ['PCE']
+task_name = 'hydration free energy'
+tasks = ['expt']
 
-raw_filename = "dataset/cep-processed.csv"
+raw_filename = "dataset/SAMPL.csv"
 feature_filename = raw_filename.replace('.csv','.pickle')
 filename = raw_filename.replace('.csv','')
-prefix_filename = raw_filename.split('/')[-1].replace('.csv','')
 
+prefix_filename = raw_filename.split('/')[-1].replace('.csv','')
 smiles_tasks_df = pd.read_csv(raw_filename)
 smilesList = smiles_tasks_df.smiles.values
 print("number of all smiles: ",len(smilesList))
-
-if os.path.isfile(feature_filename):
-    feature_dicts = pickle.load(open(feature_filename, "rb" ))
-else:
-    feature_dicts = save_smiles_dicts(smilesList,filename)
 
 atom_num_dist = []
 remained_smiles = []
@@ -77,6 +72,8 @@ for smiles in smilesList:
 
 print("number of successfully processed smiles: ", len(remained_smiles))
 smiles_tasks_df = smiles_tasks_df[smiles_tasks_df["smiles"].isin(remained_smiles)]
+
+# print(smiles_tasks_df)
 smiles_tasks_df['cano_smiles'] = canonical_smiles_list
 
 plt.figure(figsize=(5, 3))
@@ -86,40 +83,51 @@ ax = sns.distplot(atom_num_dist, bins=28, kde=False)
 plt.tight_layout()
 plt.savefig("atom_num_dist_"+prefix_filename+".png",dpi=200)
 
-random_seed = 888
+random_seed = 8 # 69, 88
 start_time = str(time.ctime()).replace(':','-').replace(' ','_')
 
 batch_size = 200
-epochs = 800
+epochs = 200
 
-p_dropout= 0.15
+p_dropout= 0.2
 fingerprint_dim = 200
 
-weight_decay = 4.5 # also known as l2_regularization_lambda
-learning_rate = 3.6
-radius = 3
-T = 1
+weight_decay = 5 # also known as l2_regularization_lambda
+learning_rate = 2.5
+output_units_num = 1 # for regression model
+radius = 2
+T = 2
 
-per_task_output_units_num = 1 # for regression model
-output_units_num = len(tasks) * per_task_output_units_num
+if os.path.isfile(feature_filename):
+    feature_dicts = pickle.load(open(feature_filename, "rb" ))
+else:
+    feature_dicts = save_smiles_dicts(smilesList,filename)
 
-# feature_dicts = get_smiles_dicts(smilesList)
 remained_df = smiles_tasks_df[smiles_tasks_df["cano_smiles"].isin(feature_dicts['smiles_to_atom_mask'].keys())]
 uncovered_df = smiles_tasks_df.drop(remained_df.index)
 
-test_df = remained_df.sample(frac=1/5,random_state=random_seed)
-train_df = remained_df.drop(test_df.index)
+print("not processed items")
+
+remained_df = remained_df.reset_index(drop=True)
+test_df = remained_df.sample(frac=1/10, random_state=random_seed) # test set
+training_data = remained_df.drop(test_df.index) # training data
+
+# training data is further divided into validation set and train set
+valid_df = training_data.sample(frac=1/9, random_state=random_seed) # validation set
+train_df = training_data.drop(valid_df.index) # train set
 
 train_df = train_df.reset_index(drop=True)
+valid_df = valid_df.reset_index(drop=True)
 test_df = test_df.reset_index(drop=True)
 
 print(len(test_df),sorted(test_df.cano_smiles.values))
 
 x_atom, x_bonds, x_atom_index, x_bond_index, x_mask, smiles_to_rdkit_list = get_smiles_array([canonical_smiles_list[0]],feature_dicts)
-loss_function = nn.MSELoss()
 
 num_atom_features = x_atom.shape[-1]
 num_bond_features = x_bonds.shape[-1]
+
+loss_function = nn.MSELoss()
 
 model = Fingerprint(radius, T, num_atom_features, num_bond_features,
             fingerprint_dim, output_units_num, p_dropout)
@@ -147,77 +155,73 @@ def train(model, dataset, optimizer, loss_function):
     model.train()
     np.random.seed(epoch)
     valList = np.arange(0,dataset.shape[0])
-
     #shuffle them
     np.random.shuffle(valList)
     batch_list = []
-
     for i in range(0, dataset.shape[0], batch_size):
         batch = valList[i:i+batch_size]
         batch_list.append(batch)   
-
-    for counter, batch in enumerate(batch_list):
-        batch_df = dataset.loc[batch,:]
+    for counter, train_batch in enumerate(batch_list):
+        batch_df = dataset.loc[train_batch,:]
         smiles_list = batch_df.cano_smiles.values
         y_val = batch_df[tasks[0]].values
         
         x_atom, x_bonds, x_atom_index, x_bond_index, x_mask, smiles_to_rdkit_list = get_smiles_array(smiles_list,feature_dicts)
         atoms_prediction, mol_prediction = model(torch.Tensor(x_atom),torch.Tensor(x_bonds),torch.cuda.LongTensor(x_atom_index),torch.cuda.LongTensor(x_bond_index),torch.Tensor(x_mask))
         
-        optimizer.zero_grad()
+        model.zero_grad()
         loss = loss_function(mol_prediction, torch.Tensor(y_val).view(-1,1))     
         loss.backward()
         optimizer.step()
-
+        
 def eval(model, dataset):
     model.eval()
-    test_MAE_list = []
-    test_MSE_list = []
+    eval_MAE_list = []
+    eval_MSE_list = []
     valList = np.arange(0,dataset.shape[0])
     batch_list = []
-
     for i in range(0, dataset.shape[0], batch_size):
         batch = valList[i:i+batch_size]
         batch_list.append(batch) 
-    for counter, batch in enumerate(batch_list):
-        batch_df = dataset.loc[batch,:]
+    for counter, eval_batch in enumerate(batch_list):
+        batch_df = dataset.loc[eval_batch,:]
         smiles_list = batch_df.cano_smiles.values
-        print(batch_df)
+#         print(batch_df)
         y_val = batch_df[tasks[0]].values
         
         x_atom, x_bonds, x_atom_index, x_bond_index, x_mask, smiles_to_rdkit_list = get_smiles_array(smiles_list,feature_dicts)
         atoms_prediction, mol_prediction = model(torch.Tensor(x_atom),torch.Tensor(x_bonds),torch.cuda.LongTensor(x_atom_index),torch.cuda.LongTensor(x_bond_index),torch.Tensor(x_mask))
         MAE = F.l1_loss(mol_prediction, torch.Tensor(y_val).view(-1,1), reduction='none')        
         MSE = F.mse_loss(mol_prediction, torch.Tensor(y_val).view(-1,1), reduction='none')
-        print(x_mask[:2],atoms_prediction.shape, mol_prediction,MSE)
+#         print(x_mask[:2],atoms_prediction.shape, mol_prediction,MSE)
         
-        test_MAE_list.extend(MAE.data.squeeze().cpu().numpy())
-        test_MSE_list.extend(MSE.data.squeeze().cpu().numpy())
-        
-    return np.array(test_MAE_list).mean(), np.array(test_MSE_list).mean()
+        eval_MAE_list.extend(MAE.data.squeeze().cpu().numpy())
+        eval_MSE_list.extend(MSE.data.squeeze().cpu().numpy())
+    return np.array(eval_MAE_list).mean(), np.array(eval_MSE_list).mean()
 
 best_param = {}
 best_param["train_epoch"] = 0
-best_param["test_epoch"] = 0
+best_param["valid_epoch"] = 0
 best_param["train_MSE"] = 9e8
-best_param["test_MSE"] = 9e8
+best_param["valid_MSE"] = 9e8
 
 # config.logger.info("Training:")
 
-for epoch in range(epochs):
+for epoch in range(800):
     train_MAE, train_MSE = eval(model, train_df)
-    test_MAE, test_MSE = eval(model, test_df)
-
+    valid_MAE, valid_MSE = eval(model, valid_df)
+#     tensorboard.add_scalars('MAE',{'train_MAE':valid_MAE, 'test_MAE':valid_MSE}, epoch)
+#     tensorboard.add_scalars('MSE',{'train_MSE':valid_MAE, 'test_MSE':valid_MSE}, epoch)
     if train_MSE < best_param["train_MSE"]:
         best_param["train_epoch"] = epoch
         best_param["train_MSE"] = train_MSE
-    if test_MSE < best_param["test_MSE"]:
-        best_param["test_epoch"] = epoch
-        best_param["test_MSE"] = test_MSE
-        if test_MSE < 0.9:
+    if valid_MSE < best_param["valid_MSE"]:
+        best_param["valid_epoch"] = epoch
+        best_param["valid_MSE"] = valid_MSE
+        if valid_MSE < 0.8:
             saved_model = 'model_'+prefix_filename+'_'+start_time+'_'+str(epoch)+'.pt'
             torch.save(model, os.path.join(wandb.run.dir, saved_model)) 
-    
+
     # config.logger.info(
     #     f"Epoch: {epoch+1} | "
     #     f"train_loss: {train_loss:.2f}, train_roc: {train_roc:.2f}, train_roc_mean: {train_roc_mean:.2f}, "
@@ -228,25 +232,25 @@ for epoch in range(epochs):
         "train_MSE": train_MSE,
         "test_MAE": test_MAE,
         "test_MSE": test_MSE})
-
-    if (epoch - best_param["train_epoch"] >2) and (epoch - best_param["test_epoch"] >18):        
-        break
     
-    print(epoch, train_MSE, test_MSE)
+    if (epoch - best_param["train_epoch"] >8) and (epoch - best_param["valid_epoch"] >18):        
+        break
+    print(epoch, np.sqrt(train_MSE), np.sqrt(valid_MSE))
+    
     train(model, train_df, optimizer, loss_function)
 
 # evaluate model
 checkpoint = 'model_'+prefix_filename+'_'+start_time+'_'+str(best_param["test_epoch"])+'.pt'
-best_model = torch.load(os.path.join(wandb.run.dir, checkpoint)) 
+best_model = torch.load(os.path.join(wandb.run.dir, checkpoint))     
 
-# best_model_dict = best_model.state_dict()
-# best_model_wts = copy.deepcopy(best_model_dict)
+best_model_dict = best_model.state_dict()
+best_model_wts = copy.deepcopy(best_model_dict)
 
-# model.load_state_dict(best_model_wts)
-# (best_model.align[0].weight == model.align[0].weight).all()
+model.load_state_dict(best_model_wts)
+(best_model.align[0].weight == model.align[0].weight).all()
 
-test_MAE, test_MSE = eval(best_model, test_df)
-print("best epoch:",best_param["test_epoch"],"\n","test MAE:",test_MAE,"\n","test MSE:",test_MSE)
+test_MAE, test_MSE = eval(model, test_df)
+print("best epoch:",best_param["valid_epoch"],"\n","test RMSE:",np.sqrt(test_MSE))
 
 # config.logger.info(
 #     "Test performance:\n"
