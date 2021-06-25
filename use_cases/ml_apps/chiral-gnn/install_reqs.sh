ea7d4791381815c7aaca83e4b30cba45c7637331
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2021-06-24 21:25:02 -0500

    initiate chiral-gnn

--- use_cases/ml_apps/chiral-gnn/install_reqs.sh
+++ use_cases/ml_apps/chiral-gnn/install_reqs.sh
@@ -1,11 +1,11 @@
 # Install dependencies / requirements
-!pip install torch==1.4.0 torchvision==0.4.0
+pip install torch==1.4.0 torchvision==0.4.0
 
-!pip install torch-geometric \
+pip install torch-geometric \
   torch-sparse==latest+cu101 \
   torch-scatter==latest+cu101 \
   torch-cluster==latest+cu101 \
   -f https://pytorch-geometric.com/whl/torch-1.4.0.html 
   
 # Install RDKit
-!pip install rdkit-pypi==2021.3.1.5 wandb
+pip install rdkit-pypi==2021.3.1.5 wandb
